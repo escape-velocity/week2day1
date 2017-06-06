17e835c2fc54619f58abec07769f94079428b79c
Bill <networker747@gmail.com>
2017-06-06 03:38:33 +0000

    pipe of request.get

--- requestHTTP.js
+++ requestHTTP.js
@@ -3,14 +3,14 @@
 var request = require('request');
 var fs = require('fs');
 
-request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
-       .on('error', function (err) {                                   // Note 2
+request.get('https://sytantris.github.io/http-examples/future.jpg')
+       .on('error', function (err) {
          throw err;
        })
-       .on('response', function (response) {                           // Note 3
+       .on('response', function (response) {
          console.log('Response Status Code: ', response.statusCode);
          console.log('Download complete.');
        })
-       .pipe(fs.createWriteStream('./future.jpg'));               // Note 4
+       .pipe(fs.createWriteStream('./future.jpg'));
         console.log('Downloading image...');
 
